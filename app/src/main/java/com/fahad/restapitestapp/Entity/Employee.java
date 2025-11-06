61b09c3ee36206283af9046cef9217f57121bf77
SM-Fahad <fahad.dev2024@gmail.com>
2025-11-06 13:06:05 +0600

    Initial Commit

---
+++ app/src/main/java/com/fahad/restapitestapp/Entity/Employee.java
@@ -0,0 +1,70 @@
+package com.fahad.restapitestapp.Entity;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private String designation;
+    private String email;
+    private Double salary;
+    private String address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", designation='" + designation + '\'' +
+                ", email='" + email + '\'' +
+                ", salary=" + salary +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
